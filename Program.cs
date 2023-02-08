3bf95a76683e48aa4a1a7633441c5d56f6f23d57
Tourya <tourya@abv.bg>
2023-02-08 12:25:37 +0200

    Models, Implement and Register the Db

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.EmployeeManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<EmployeeManagerDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("EmployeeManagerDb")));
 
 var app = builder.Build();
 
